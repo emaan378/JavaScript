bcaadfe5c3067ea96ed7a375bafc9a97bcadf83e
Emaan Fatima <alizbat14@gmail.com>
2026-03-24 23:51:57 +0500

    the project complete

--- Bussiness Name Generator/index.js
+++ Bussiness Name Generator/index.js
@@ -12,3 +12,38 @@
 // Bros
 // Limited
 // Hub
+let rand = Math.random();
+let first, second, third;
+// lets generate the first word
+if (rand < 0.33) {
+    first = "crazy";
+}
+else if(rand < 0.66 && rand > 0.99){
+    first = "Amazing";
+}
+else {
+    first = "Fire";
+}
+rand = Math.random();
+// lets generate the second word
+if (rand < 0.33) {
+    second = "Guarments";
+}
+else if(rand < 0.66 && rand > 0.99){
+    second = "Engine";
+}
+else {
+    second = "Food";
+}
+rand = Math.random();
+// lets generate the third word
+if (rand < 0.33) {
+    third = "Bros";
+}
+else if(rand < 0.66 && rand > 0.99){
+    third = "Limited";
+}
+else {
+    third = "Hub";
+}
+console.log(`${first} ${second} ${third}`)
