a4a2647bd7fd9f725ecd338a42d63bb0e053df5c
Emaan Fatima <alizbat14@gmail.com>
2026-03-24 23:35:37 +0500

    Add new logics

---
+++ Bussiness Name Generator/index.js
@@ -0,0 +1,14 @@
+// Create a bussiness name Generator by combining list of objectives and shop name and other word
+// Crazy
+// Amazing
+// Fire
+
+// Shop Name
+// Guarments
+// Engine
+// Food
+
+// Another Words
+// Bros
+// Limited
+// Hub
